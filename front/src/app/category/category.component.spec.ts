1234b8f17a0aa83f79760fc326fdd805a27151ca
webprojectaccount <81443432+webprojectaccount@users.noreply.github.com>
2021-04-27 19:43:24 +0600

    Update category.component.spec.ts

--- front/src/app/category/category.component.spec.ts
+++ front/src/app/category/category.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'; а
 
 import { CategoryComponent } from './category.component';
 
