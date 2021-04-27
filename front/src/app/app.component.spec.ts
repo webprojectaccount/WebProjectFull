b43754d751df6100817445c78f8940eeb16d2d1d
webprojectaccount <81443432+webprojectaccount@users.noreply.github.com>
2021-04-27 19:50:07 +0600

    Update app.component.spec.ts

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing'; f
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
