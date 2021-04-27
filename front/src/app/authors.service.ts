779cf99143724bc6a99892f5834bead050b75422
webprojectaccount <81443432+webprojectaccount@users.noreply.github.com>
2021-04-27 19:49:40 +0600

    Update authors.service.ts

--- front/src/app/authors.service.ts
+++ front/src/app/authors.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core'; f
+import { Injectable } from '@angular/core'; 
 import {Observable, of} from 'rxjs';
 import {Authors} from './models';
 import {authors} from './autors';
