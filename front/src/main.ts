e01bf416e436814567182274c193dfe3d2e5942e
webprojectaccount <81443432+webprojectaccount@users.noreply.github.com>
2021-04-27 19:37:18 +0600

    Update main.ts

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode } from '@angular/core'; а
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { environment } from './environments/environment';
 
